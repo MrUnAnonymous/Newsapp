a6613bd55ce30d9501bd8c8b62ba5caa9ff6ce9a
MrUmAnonymous <saurabhsapkal16@gmail.com>
2022-08-01 16:48:53 +0530

    ai added

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,6 +1,19 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import alanBtn from '@alan-ai/alan-sdk-web';
+const alanKey = 'bc2ddd4c6c5ac601b9fbe3ca963ad7522e956eca572e1d8b807a3e2338fdd0dc/stage';
 const App = () => {
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            oncommand: ({command}) => {
+                if(command === 'testCommand') {
+                    alert('this code was executed');
+                }
+            }
+        })
+
+    }, [])
     return(
         <div>
             <h1>Anonymous News App</h1>
