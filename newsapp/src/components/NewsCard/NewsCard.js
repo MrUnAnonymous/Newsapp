55fde2313f92cc809338226c1eb70992f4cbcdca
MrUnAnonymous <saurabhsapkal16@gmail.com>
2022-09-03 14:01:53 +0530

    newscard added w/ bugs

--- newsapp/src/components/NewsCard/NewsCard.js
+++ newsapp/src/components/NewsCard/NewsCard.js
@@ -1,8 +1,28 @@
 import React from "react";
+import { Card , CardActions , CardActionArea , CardContent , CardMedia , Button , Typography  } from "@material-ui/core";
 
-const NewsCard = () => {
+import useStyles from './styles.js';
+
+const NewsCard = ({ articles:{ description , publishedAt , source , title , url , urlToImage } , i}) => {
+    const classes = useStyles();
     return(
-        <div></div>
+        <Card className={classes.card}>
+            <CardActionArea href={url} target="_blank">
+                <CardMedia className={classes.media} image = {urlToImage || 'https://www.pngwing.com/en/free-png-taelk'} />
+                    <div className={classes.details}>
+                        <Typography varient="body2" color="textSecondary" component='h2'>{(new Date(publishedAt)).toDateString()}</Typography>
+                        <Typography varient="body2" color="textSecondary" component='h2'>{source.name}</Typography>
+                    </div>
+                    <Typography className={classes.title} gutterBottom variant="h5">{title}</Typography>
+                    <CardContent>
+                        <Typography varient="body2" color="textSecondary" component='p'>{description}</Typography>
+                    </CardContent>
+            </CardActionArea>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary">LEARN MORE</Button>
+                <Typography varient="body2" color="textSecondary">{i+1}</Typography>
+            </CardActions>
+        </Card>
     )
 }
 
